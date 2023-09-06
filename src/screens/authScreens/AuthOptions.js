434563ae26fc92835d51de5a205c92005c57d691
Max Bueckert <maxbueckert@gmail.com>
2023-09-06 11:28:24 -0700

    auth final

--- src/screens/authScreens/AuthOptions.js
+++ src/screens/authScreens/AuthOptions.js
@@ -3,16 +3,17 @@ import {View} from 'react-native';
 import { Button } from 'react-native-paper';
 
 import Header from '../../components/header/Header.js';
-
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 const MyComponent = ({navigation}) => (
     <View style = {{flex:1}}>
     <Header></Header>
-    <View style={{ padding: 20, flex : 1, alignItems : 'center', justifyContent : 'space-around' }}>
-        <Button icon="camera" mode="contained" onPress={() => navigation.navigate("SignInScreen")}>
+    <View style={{ padding: 20, flex : 1, alignItems : 'center', justifyContent : 'space-evenly' }}>
+        <Button contentStyle = {{ width: 300, height: 100}} icon="account" mode="contained" onPress={() => navigation.navigate("SignInScreen")}>
             Sign In
         </Button>
-            <Button icon="camera" mode="contained" onPress={() => navigation.navigate("SignUpScreen")}>
+            <Button  contentStyle = {{ width: 300, height: 100}} icon="account-plus" mode="contained" onPress={() => navigation.navigate("SignUpScreen")}>
             Sign Up
         </Button>
   </View>
