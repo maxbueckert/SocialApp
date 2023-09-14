ace7541e858505c9ce872ea29a8efb8e4a3aee04
Alberto Yanes <albertoyanesgca@gmail.com>
2023-09-13 17:33:35 -0700

    updated to use subscription

--- src/hooks/addUserAttribute.js
+++ src/hooks/addUserAttribute.js
@@ -1,10 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { API, graphqlOperation, Auth } from 'aws-amplify';
 import { updateUsers } from '../graphql/mutations';
+import { onUpdateUsers } from '../graphql/subscriptions';
 
 const addUserAttributes = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    // Set up subscription to listen for user updates
+    const userSubscription = API.graphql(
+      graphqlOperation(onUpdateUsers)
+    ).subscribe({
+      next: (data) => {
+        console.log('User attributes updated:', data);
+        // Handle the updated user attributes as needed
+      },
+      error: (subscriptionError) => {
+        console.error('User attributes subscription error:', subscriptionError);
+      }
+    });
+
+    // Clean up the subscription on unmount
+    return () => {
+      userSubscription.unsubscribe();
+    };
+  }, []);
 
   const updateUserAttribute = async (attributeValue) => {
     try {
